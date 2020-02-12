0e30373143a96da68e845fff42db7b012e8c52ad
bzq360 <2733938791@qq.com>
2020-02-12 19:44:35 +0000

    ...

--- src/package2/Bulldog.java
+++ src/package2/Bulldog.java
@@ -7,12 +7,7 @@ import package1.Fish;
 public class Bulldog extends Animal {
 
     void bark() {
-
         Leg leg = new Dog().getLeg(new Fish(), new Fish().getSpeed(), new Fish().SPEED, 777, "str");
     }
 
-    class FatBullDog {
-        void run() {
-        }
-    }
 }
